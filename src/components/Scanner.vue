eb094020954cc97b1a38a3fa5bfa4de931812180
yazid <yaziddunda@gmail.com>
2019-05-28 23:36:04 +0700

    Add spinner, and #1 try hit the API

--- src/components/Scanner.vue
+++ src/components/Scanner.vue
@@ -1,29 +1,67 @@
 <template>
-  <div>
+  <div class="scanner">
     <qrcode-stream @decode="onDecode"></qrcode-stream>
-    <p>{{ decodedString }}</p>
+    <pulse-loader v-if="loading == true"></pulse-loader>
+    <p> {{ successMsg  }} </p>
+    <p>{{ errorMsg }}</p>
   </div>
 </template>
 
 <script>
 import  { QrcodeStream } from 'vue-qrcode-reader'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import axios from 'axios'
+
 export default {
   name: 'Scanner',
   data() {
     return {
-      decodedString: ''
+      decodedString: '',
+      loading: false,
+      name: "Ghiant Masua Khols",
+      userId: 400364,
+      successMsg: '',
+      errorMsg: ''
     }
   },
   components: {
     QrcodeStream,
+    PulseLoader
   },
   methods: {
     onDecode(decoded) {
-      this.decodedString = decoded;
+      this.loading = true;
+      let mahasiswa = {
+        name: this.name,
+        userId: this.userId,
+        presenceId: decoded.presenceId,
+        presencenumber: decoded.presencenumber
+      }
+
+      axios({
+        method: 'post',
+        url: 'https://fathomless-hollows-37188.herokuapp.com/users/presence',
+        data: mahasiswa
+      })
+      .then(response => {
+        this.loading = false;
+        this.successMsg = response;
+      })
+      .catch(error => {
+        this.errorMsg = error;
+      })
+
+
     }
   }
 }
 </script>
 
 <style scoped>
+
+.scanner {
+  text-align: center;
+}
+
+
 </style>
