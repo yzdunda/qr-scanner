d97afb8d4ac07681c62d779f016967a94006a19e
yazid <yaziddunda@gmail.com>
2019-05-28 15:10:12 +0700

    Init basic qrscan

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Scanner />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Scanner from './components/Scanner.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Scanner
   }
 }
 </script>
---
+++ src/components/Scanner.vue
@@ -0,0 +1,16 @@
+<template>
+  <qrcode-stream @decode="onDecode"></qrcode-stream>
+</template>
+
+<script>
+import  { QrcodeStream } from 'vue-qrcode-reader'
+export default {
+  name: 'Scanner',
+  components: {
+    QrcodeStream,
+  }
+}
+</script>
+
+<style scoped>
+</style>
