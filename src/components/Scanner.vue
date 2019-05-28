53b66cad60daad1ef6605bdb79fc5dc20d0ef1da
yazid <yaziddunda@gmail.com>
2019-05-28 15:29:30 +0700

    Test payload from QR

--- src/components/Scanner.vue
+++ src/components/Scanner.vue
@@ -1,13 +1,26 @@
 <template>
-  <qrcode-stream @decode="onDecode"></qrcode-stream>
+  <div>
+    <qrcode-stream @decode="onDecode"></qrcode-stream>
+    <p>{{ decodedString }}</p>
+  </div>
 </template>
 
 <script>
 import  { QrcodeStream } from 'vue-qrcode-reader'
 export default {
   name: 'Scanner',
+  data() {
+    return {
+      decodedString: ''
+    }
+  },
   components: {
     QrcodeStream,
+  },
+  methods: {
+    onDecode(decoded) {
+      this.decodedString = decoded;
+    }
   }
 }
 </script>
